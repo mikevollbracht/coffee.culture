82af5db1d44814649eb30d7a1e49f03795892e79
Mike Vollbracht <mike.vollbracht@gmail.com>
2018-06-23 12:31:04 -0500

    First Commit

---
+++ javascript/fb-menubar.js
@@ -0,0 +1,19 @@
+$( document ).ready(function(){
+
+	// Open mobile nav
+	$("#mobile-menu-open").click(function(){
+		$("#mobile-menu-list").removeClass("d-none");
+	});
+
+	// Close mobile nav 
+	$("#mobile-menu-close").click(function(){
+		$("#mobile-menu-list").addClass("d-none");
+	});
+
+	
+
+
+
+}) 
+	
+
